da5de7cd949dc2bdcc660d2c82079da4e1ab32dd
Idrees Khan <dev.idreeskhan@gmail.com>
2024-11-21 19:35:37 +0100

    feat(cleanup memory)

--- android/src/main/java/com/capacitor/plugin/niimbot/NiimbotPrinterPlugin.java
+++ android/src/main/java/com/capacitor/plugin/niimbot/NiimbotPrinterPlugin.java
@@ -14,10 +14,13 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import android.widget.FrameLayout;
 import android.view.Gravity;
 import android.view.ViewGroup;
-import android.widget.Toast;
+import android.graphics.Color;
+import android.content.res.ColorStateList;
 
 @CapacitorPlugin(name = "NiimbotPrinter")
 public class NiimbotPrinterPlugin extends Plugin {
+
+    private FloatingActionButton _fab;
 
     @PluginMethod
     public void echo(PluginCall call) {
@@ -45,7 +48,8 @@ public class NiimbotPrinterPlugin extends Plugin {
 
         Handler mainHandler = new Handler(Looper.getMainLooper());
         mainHandler.post(() -> {
-            WebView webView = this.bridge.getWebView();
+            final WebView webView = this.bridge.getWebView();
+
             final String currentUrl = webView.getUrl();
 
             final String baseUrl = currentUrl.substring(0, currentUrl.lastIndexOf("/")) + "/";
@@ -54,37 +58,53 @@ public class NiimbotPrinterPlugin extends Plugin {
 
             // Set a WebViewClient to listen for the page load completion
             webView.setWebViewClient(new WebViewClient() {
+                private boolean isPageLoaded = false;
+
                 @Override
                 public void onPageFinished(WebView view, String url) {
+                    if(isPageLoaded) {
+                        // Remove the WebViewClient to prevent multiple calls after redirect to main app
+                        webView.setWebViewClient(null);
+                        return;
+                    }
+
+                    isPageLoaded = true;
                     super.onPageFinished(view, url);
 
-                    final String previewScript = "var toolbarButtons = document.querySelectorAll('.toolbar > button');" +
-                        "console.log(toolbarButtons);" +
-                        "var previewButton = null;" +
-                        "toolbarButtons.forEach(function(button) {" +
-                        "    var icon = button.querySelector('span.mdi');" +
-                        "    if (icon && icon.textContent === '') {" +
-                        "        previewButton = button;" +
-                        "    }" +
-                        "});" +
-                        "console.log('previewButton', previewButton);" +
-                        "if (previewButton) {" +
-                        "    previewButton.click();" +
-                        "}";
-
-                    final String redirectScript = "var button = document.querySelector('.modal .btn-secondary');" +
-                            "if (button) {" +
-                            "    button.addEventListener('click', function() {" +
-                            "        if(window.Capacitor.Plugins.SplashScreen) {" +
-                            "           window.Capacitor.Plugins.SplashScreen.show({ autoHide: false });" +
-                            "        }" +
-                            "        window.WebAppInterface.onCloseButtonClick('" + finalToastMessage + "');" +
-                            "        window.location.href = '" + redirectFullUri + "';" +
-                            "    });" +
+                    final String previewScript = "window.Capacitor.Plugins.SplashScreen?.hide();" +
+                            "var toolbarButtons = document.querySelectorAll('.toolbar > button');" +
+                            "console.log('toolbarButtons', toolbarButtons);" +
+                            "var previewButton = null;" +
+                            "toolbarButtons.forEach(function(button) {" +
+                            "    var icon = button.querySelector('span.mdi');" +
+                            "    if (icon && icon.textContent === '') {" +
+                            "        previewButton = button;" +
+                            "    }" +
+                            "});" +
+                            "console.log('previewButton', previewButton);" +
+                            "if (previewButton) {" +
+                            "    previewButton.click();" +
                             "}";
+
+                    final String redirectScript =
+                            "var checkButtonInterval = setInterval(function() {" +
+                                    "    var button = document.querySelector('.modal .btn-secondary');" +
+                                    "    console.log('Checking for button:', button);" +
+                                    "    if (button) {" +
+                                    "        clearInterval(checkButtonInterval);" +
+                                    "        console.log('Button found:', button);" +
+                                    "        button.addEventListener('click', function() {" +
+                                    "            console.log('Calling onCloseButtonClick');" +
+                                    "            window.WebAppInterface.onCloseButtonClick('" + finalToastMessage + "', '" + redirectFullUri + "');" +
+                                    "        });" +
+                                    "    }" +
+                                    "}, 100);"; // Check every 100 milliseconds
                     final String fullScript = (preview ? previewScript : "") + redirectScript;
                     // Inject JavaScript to set up the event listener after the page loads
                     webView.evaluateJavascript(fullScript, null);
+
+                    // Add the FloatingActionButton
+                    addFloatingActionButton(webView, finalToastMessage, redirectFullUri);
                 }
             });
 
@@ -92,9 +112,6 @@ public class NiimbotPrinterPlugin extends Plugin {
             webView.loadUrl(niimblueFullUri);
             // Add the JavaScript interface
             webView.addJavascriptInterface(new WebAppInterface(this), "WebAppInterface");
-
-            // Add the FloatingActionButton
-            addFloatingActionButton(webView, finalToastMessage, redirectFullUri);
 
             call.resolve();
         });
@@ -106,17 +123,19 @@ public class NiimbotPrinterPlugin extends Plugin {
 
     private void addFloatingActionButton(WebView webView, String finalToastMessage, String redirectFullUri) {
         // Create a FloatingActionButton
-        FloatingActionButton fab = new FloatingActionButton(this.getContext());
-        fab.setImageResource(android.R.drawable.ic_menu_close_clear_cancel); // Set the close icon
-        fab.setOnClickListener(view -> {
+        _fab = new FloatingActionButton(this.getContext());
+        _fab.setImageResource(android.R.drawable.ic_menu_close_clear_cancel); // Set the close icon
+
+        // Set the background color of the FAB using a hex value
+        _fab.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#000000"))); // Change the hex value as needed
+
+        _fab.setOnClickListener(view -> {
             // Handle FAB click
-            //Toast.makeText(this.getContext(), "FAB Clicked", Toast.LENGTH_SHORT).show();
-            // Call the onCloseButtonClick method from the JavaScript interface
-            final String script = "window.WebAppInterface.onCloseButtonClick('" + finalToastMessage + "');" +
-            "        window.location.href = '" + redirectFullUri + "';";
-            webView.evaluateJavascript(script, null);
+            final String script =
+                    "window.WebAppInterface.onCloseButtonClick('" + finalToastMessage + "', '" + redirectFullUri + "');";
+            this.bridge.getWebView().evaluateJavascript(script, null);
         });
-    
+
         // Set layout parameters for the FAB
         FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(
                 FrameLayout.LayoutParams.WRAP_CONTENT,
@@ -125,22 +144,34 @@ public class NiimbotPrinterPlugin extends Plugin {
         params.gravity = Gravity.BOTTOM | Gravity.END;
         int margin = (int) (16 * getContext().getResources().getDisplayMetrics().density); // Convert 16dp to pixels
         params.setMargins(margin, margin, margin, margin);
-        fab.setLayoutParams(params);
-    
+        _fab.setLayoutParams(params);
+
         // Ensure the parent layout is a FrameLayout
         ViewGroup parent = (ViewGroup) webView.getParent();
         if (parent instanceof FrameLayout) {
             // Add the FAB to the WebView's parent layout
-            parent.addView(fab);
+            parent.addView(_fab);
         } else {
             // Wrap the WebView in a FrameLayout
             FrameLayout frameLayout = new FrameLayout(this.getContext());
             parent.removeView(webView);
             frameLayout.addView(webView);
-            frameLayout.addView(fab);
+            frameLayout.addView(_fab);
             parent.addView(frameLayout);
         }
     }
+
+
+    public void removeFloatingActionButton() {
+        if (_fab != null) {
+            ViewGroup parent = (ViewGroup) _fab.getParent();
+            if (parent != null) {
+                parent.removeView(_fab);
+            }
+            _fab = null;
+        }
+    }
+
 //    public void notifyDone() {
 //        notifyListeners("done", null);
 //    }
--- android/src/main/java/com/capacitor/plugin/niimbot/WebAppInterface.java
+++ android/src/main/java/com/capacitor/plugin/niimbot/WebAppInterface.java
@@ -1,5 +1,7 @@
 package com.capacitor.plugin.niimbot;
 
+import android.os.Handler;
+import android.os.Looper;
 import android.webkit.JavascriptInterface;
 import android.widget.Toast;
 
@@ -13,11 +15,29 @@ public class WebAppInterface {
     }
 
     @JavascriptInterface
-    public void onCloseButtonClick(final String toastMessage) {
-        if(!toastMessage.isEmpty()) {
-            Toast.makeText(plugin.getContext(), toastMessage, Toast.LENGTH_SHORT).show();
-        }
+    public void onCloseButtonClick(final String toastMessage, final String redirectFullUri) {
+        Handler mainHandler = new Handler(Looper.getMainLooper());
+        mainHandler.post(() -> {
+            final String script =
+                    "if(window.Capacitor.Plugins.SplashScreen) {" +
+                        "console.log('Showing SplashScreen');" +
+                        "window.Capacitor.Plugins.SplashScreen.show({ autoHide: false });" +
+                    "}";
+            plugin.getBridge().getWebView().evaluateJavascript(script, null);
+
+            if (plugin instanceof NiimbotPrinterPlugin) {
+                ((NiimbotPrinterPlugin) plugin).removeFloatingActionButton();
+            }
+
+            if (!toastMessage.isEmpty()) {
+                Toast.makeText(plugin.getContext(), toastMessage, Toast.LENGTH_SHORT).show();
+            }
+
+            // Redirect to the specified URI
+            plugin.getBridge().getWebView().loadUrl(redirectFullUri);
+        });
     }
+
 
 //    @JavascriptInterface
 //    public void onDone() {
